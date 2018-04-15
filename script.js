a21f4fae8a9a5a56ae8f9f2d6279fe50491c64ed
Dominik Broza <domibroz@gmail.com>
2018-04-15 21:25:49 +0200

    3

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ $(document).ready(function() {
     var requestUrl = apiRoot + 'tasks';
 
     $.ajax({
-      url: requestUrl + '/' + taskId.id
+      url: requestUrl + '/' + ${taskId}
       }),
       method: 'DELETE',
       success: function() {
