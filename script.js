6889572768e3dafd16fcd52af7bc3306a9cac4e0
Dominik Broza <domibroz@gmail.com>
2018-04-15 20:46:59 +0200

    script delete update

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ $(document).ready(function() {
 
     $.ajax({
       url: requestUrl + '/' + $.param({
-        taskId: taskId
+        taskId
       }),
       method: 'DELETE',
       success: function() {
@@ -140,7 +140,7 @@ $(document).ready(function() {
         content: taskContent
       }),
       complete: function(data) {
-        if(data.status === 200) {
+        if(data.status == 200) {
           getAllTasks();
         }
       }
